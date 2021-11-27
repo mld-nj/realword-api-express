c6bd4d0761dad5f8e3bee383cc70125e7799996a
lili kai <1761311608@qq.com>
2021-11-27 13:19:35 +0800

    add middleware

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
-
+app.use(cors);
 app.get("/", (req, res) => {
   res.send("hello world");
 });
